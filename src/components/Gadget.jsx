7bc5cc830dbad4c4b8e48d49c86dff59b92ef725
ZaidRasheed <94339310+ZaidRasheed@users.noreply.github.com>
2022-07-13 04:22:40 -0400

    pre

--- src/components/Gadget.jsx
+++ src/components/Gadget.jsx
@@ -13,40 +13,13 @@ function Gadget(props) {
 
     useEffect(() => {
         setExpanded(props.icon === null ? "" : "expanded")
-        setError(props.icon === null && searched ? "404 Error location not found" : "")
-    }, [props.icon,searched])
+        setError(props.icon === null && searched ? "Error location not found" : "")
+    }, [props.icon, searched])
     const element = (
         <div>
             <div className="error">
-                <span>{error}</span>
+                <p>{error}</p>
             </div>
-
-            <div id="expand-container">
-                <div className={`main ${expanded}`} id="expand-contract">
-                    <div>
-                        <div className="level1">
-                            <div className="img">
-                                <img src={`https://openweathermap.org/img/wn/${props.icon}@4x.png`} />
-                            </div>
-                            <div className="basic">
-                                <h1>{props.cityName}, {props.country}</h1>
-                                <span className="temp">{parseFloat(props.temp).toFixed(1)}°</span>
-                            </div>
-
-                        </div>
-                        <div className="level2">
-                            <h2>
-                                {capitalize(props.description)}
-                            </h2>
-                            <h2>
-                                Real feel {parseFloat(props.feels_like).toFixed(1)}°
-                            </h2>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-
             <div className="bottom">
 
                 <div className="bySearch">
@@ -69,7 +42,7 @@ function Gadget(props) {
                             <span className="field__label">City name</span>
                         </span>
                     </div>
-                    <button className="button bySearchButton" onClick={()=>{
+                    <button className="button bySearchButton" onClick={() => {
                         props.bySearch();
                         setSearched(true);
                     }} >Search</button>
@@ -78,6 +51,33 @@ function Gadget(props) {
                     <button className="button" onClick={props.getCurrentLocation}>Current location</button>
                 </div>
             </div>
+            <div id="expand-container">
+                <div className={`main ${expanded}`} id="expand-contract">
+                    <div>
+                        <div className="level1">
+                            <div className="img">
+                                <img alt="weather icon" src={`https://openweathermap.org/img/wn/${props.icon}@4x.png`} />
+                            </div>
+                            <div className="basic">
+                                <h1>{props.cityName}, {props.country}</h1>
+                                <span className="temp">{parseFloat(props.temp).toFixed(1)}°</span>
+                            </div>
+
+                        </div>
+                        <div className="level2">
+                            <h2>
+                                {capitalize(props.description)}
+                            </h2>
+                            <h2>
+                                Real feel {parseFloat(props.feels_like).toFixed(1)}°
+                            </h2>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
         </div>
     )
 
